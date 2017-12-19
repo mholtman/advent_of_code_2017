01dfb6aa9552d0e76177c5d9e449b1cbc416cef1
Mark Holtman <mholtman@gmail.com>
2017-12-19 10:33:38 -0500

    Day Four, Part Two done

--- lib/advent_of_code_day_four.ex
+++ lib/advent_of_code_day_four.ex
@@ -31,4 +31,13 @@ defmodule AoC.DayFour do
 
     number_of_words == number_of_non_anagrams
   end
+
+  def number_of_non_anagram_passphrases_in_file(file_path) do
+    file_path
+    |> File.read!
+    |> String.split("\n")
+    |> List.delete_at(-1)
+    |> Enum.filter(fn(x) -> is_super_valid_passphrase(x) end)
+    |> length
+  end
 end
