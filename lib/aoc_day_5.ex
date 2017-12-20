a2b759839ac937add0643e7363c08bc5f9f1a237
Mark Holtman <mholtman@gmail.com>
2017-12-20 13:07:20 -0500

    Added accumulator to track steps

--- lib/aoc_day_5.ex
+++ lib/aoc_day_5.ex
@@ -1,17 +1,15 @@
 defmodule AoC.DayFive do
 
-  def jump_from_index(list, index) when index < length(list) do
+  def jump_from_index(list, acc, index) when index < length(list) do
     current_value = Enum.at(list, index)
 
     new_list = List.replace_at(list, index, current_value + 1)
 
-    {:ok, new_list, current_value + index}
+    {:ok, new_list, acc + 1, current_value + index}
   end
 
-  def jump_from_index(list, index) when index >= length(list) do
-    {:escaped_minotaur, list, index}
+  def jump_from_index(list, acc, index) when index >= length(list) do
+    {:escaped_minotaur, list, acc, index}
   end
 
-  def escape_the_maze(list) do
-  end
 end
--- test/aoc_day_5_test.exs
+++ test/aoc_day_5_test.exs
@@ -2,19 +2,19 @@ defmodule AoC.DayFive.Test do
   use ExUnit.Case
 
   test "jumping from head of a list increments the value" do
-    assert(AoC.DayFive.jump_from_index([0, 1, 2, 3], 0) == {:ok, [1, 1, 2, 3], 0})
+    assert(AoC.DayFive.jump_from_index([0, 1, 2, 3], 0, 0) == {:ok, [1, 1, 2, 3], 1, 0})
   end
 
   test "jumping from mid list increments value" do
-    assert(AoC.DayFive.jump_from_index([0, 1, 2, 3], 1) == {:ok, [0, 2, 2, 3], 2})
+    assert(AoC.DayFive.jump_from_index([0, 1, 2, 3], 0, 1) == {:ok, [0, 2, 2, 3], 1, 2})
   end
 
   test "jumping out of the list escapes the minotaur" do
-    assert(AoC.DayFive.jump_from_index([0, 1, 2, 3], 2) == {:ok, [0, 1, 3, 3], 4})
+    assert(AoC.DayFive.jump_from_index([0, 1, 2, 3], 0, 2) == {:ok, [0, 1, 3, 3], 1, 4})
   end
 
   test "passing an index out of bounds escapes the minotaur" do
-    assert(AoC.DayFive.jump_from_index([0, 1, 3, 3], 4) == {:escaped_minotaur, [0, 1, 3, 3], 4})
+    assert(AoC.DayFive.jump_from_index([0, 1, 3, 3], 0, 4) == {:escaped_minotaur, [0, 1, 3, 3], 0, 4})
   end
 
 end
