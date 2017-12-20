3e8fd0f35b0b4a3d90c620f16a51fc2fdbb70398
Mark Holtman <mholtman@gmail.com>
2017-12-20 13:14:20 -0500

    refactored down to one function head

--- lib/aoc_day_5.ex
+++ lib/aoc_day_5.ex
@@ -1,15 +1,16 @@
 defmodule AoC.DayFive do
 
-  def jump_from_index(list, acc, index) when index < length(list) do
-    current_value = Enum.at(list, index)
+  def jump_from_index(list, acc, index) do
+    if index < length(list) do
+      current_value = Enum.at(list, index)
 
-    new_list = List.replace_at(list, index, current_value + 1)
+      new_list = List.replace_at(list, index, current_value + 1)
 
-    {:ok, new_list, acc + 1, current_value + index}
+      {:ok, new_list, acc + 1, current_value + index}
+
+    else
+      {:escaped_minotaur, list, acc, index}
+    end
+
   end
-
-  def jump_from_index(list, acc, index) when index >= length(list) do
-    {:escaped_minotaur, list, acc, index}
-  end
-
 end
